a1910579d40c0b24848b9693dd745bef5750f5ac
Kanyinsola Fapohunda <kanyinsolafapohunda@gmail.com>
2018-04-15 14:24:20 +0100

    Initial Commit

---
+++ iOSBaseProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSBaseProject
+//
+//  Created by codedentwickler on 4/15/18.
+//  Copyright © 2018 Cotta & Cush. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
